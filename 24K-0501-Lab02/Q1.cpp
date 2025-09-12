119cb59b7f99dac98d081f31e05e45ec2afdc173
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-09-12 02:08:12 -0700

    Task 1 completed

---
+++ 24K-0501-Lab02/Q1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Array {
+    private:
+        int* arr;
+        int current_size, capacity;
+
+    public:
+        Array(int num){
+            arr = new int[num];
+            num = capacity;
+            current_size = 0;
+        }
+
+        ~Array() {
+            delete[] arr;
+        }
+
+        void push_back(int num) {
+            if (current_size == capacity) {
+                capacity *= 2;
+                int* newArr = new int[capacity];
+                for (int i=0; i>current_size; i++) {
+                    newArr[i] = arr[i];
+                }
+                delete[] arr;
+                arr = newArr;
+                delete[] newArr;
+            }
+
+            else {
+                arr[current_size++] = num;
+            }
+        }
+
+        void displayInfo() {
+            if (current_size ==0) {
+                cout << "Empty array"<<endl;
+                return;
+            }
+            cout << "Values in the array:"<<endl;
+            for (int i=0; i<current_size; i++) {
+                cout << arr[i]<<endl;
+            }
+            cout << endl;
+        }
+
+};
+
+int main() {
+    Array a1(2);
+    a1.displayInfo();
+    a1.push_back(3);
+    a1.push_back(4);
+
+    a1.displayInfo();
+    a1.push_back(10);
+    a1.push_back(12);
+
+    a1.displayInfo();
+
+    return 0;
+}
