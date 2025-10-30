61b0b1cdb7b2f2c22f18605f95ee7357076d1d1e
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-10-30 21:25:06 +0500

    Lab07 task 5 completed

--- 24K-0501-Lab07/Q5.cpp
+++ 24K-0501-Lab07/Q5.cpp
@@ -31,15 +31,22 @@ void quickSort(int arr[], int start, int end, string method) {
     if (start < end) {
         int pivotIndex = end;
 
-        if (method == "first") pivotIndex = start;
-        else if (method == "random") pivotIndex = start + rand() % (end - start + 1);
-        else if (method == "middle") pivotIndex = start + (end - start) / 2;
-        else if (method == "median3") {
+        if (method == "first") {
+            pivotIndex = start;
+        } else if (method == "random") {
+            pivotIndex = start + rand() % (end - start + 1);
+        } else if (method == "middle") {
+            pivotIndex = start + (end - start) / 2;
+        } else if (method == "median3") {
             int mid = start + (end - start) / 2;
             int a = arr[start], b = arr[mid], c = arr[end];
-            if ((a > b) != (a > c)) pivotIndex = start;
-            else if ((b > a) != (b > c)) pivotIndex = mid;
-            else pivotIndex = end;
+            if ((a > b) != (a > c)) {
+                pivotIndex = start;
+            } else if ((b > a) != (b > c)) {
+                pivotIndex = mid;
+            } else {
+                pivotIndex = end;
+            }
         }
 
         int p = partition(arr, start, end, pivotIndex);
@@ -49,13 +56,15 @@ void quickSort(int arr[], int start, int end, string method) {
 }
 
 void copyArray(int source[], int dest[], int n) {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         dest[i] = source[i];
+    }
 }
 
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
+    }
     cout << endl;
 }
 
@@ -68,10 +77,10 @@ int main() {
 
     int arr[n], temp[n];
     cout << "Enter " << n << " integers:" << endl;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
+    }
 
-        
     copyArray(arr, temp, n);
     compCount = 0;
     quickSort(temp, 0, n - 1, "first");
@@ -86,7 +95,6 @@ int main() {
     cout << "Sorted array: "; printArray(temp, n);
     cout << "Number of comparisons: " << compCount << endl;
 
-    
     copyArray(arr, temp, n);
     compCount = 0;
     quickSort(temp, 0, n - 1, "middle");
@@ -94,7 +102,6 @@ int main() {
     cout << "Sorted array: "; printArray(temp, n);
     cout << "Number of comparisons: " << compCount << endl;
 
-    
     copyArray(arr, temp, n);
     compCount = 0;
     quickSort(temp, 0, n - 1, "median3");
