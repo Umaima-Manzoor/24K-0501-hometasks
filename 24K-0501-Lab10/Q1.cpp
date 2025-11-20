d77c9409bf145572248806743373f78bd1f51d37
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-11-20 15:45:50 +0500

    Create Q1.cpp

---
+++ 24K-0501-Lab10/Q1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int capacity = 5;
+int heap[5];
+int num = 0;
+
+
+int parent(int i) {
+    return (i-1)/2;
+}
+
+int leftChild(int i) {
+    return (2*i) + 1;
+}
+
+int rightChild(int i) {
+    return (2*i) + 2;
+}
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+
+void minHeapify(int i) {
+    int smallest = i;
+    int left = leftChild(i);
+    int right = rightChild(i);
+
+    if (left<num && heap[left] < heap[smallest])
+        smallest = left;
+    if (right <num && heap[right] < heap[smallest])
+        smallest = right;
+
+    if (smallest != i) {
+        swap(heap[i], heap[smallest]);
+        minHeapify(smallest);
+    }
+}
+
+
+
+void insert(int val) {
+    if (num==capacity) {
+        cout << "Heap is full!" <<endl;
+        return;
+    }
+    heap[num] = val;
+    num++;
+
+    int i=num -1;
+    while (i!=0 && heap[parent(i)] > heap[i]) {
+        swap(heap[i], heap[parent(i)]);
+        i = parent(i);
+    }
+}
+
+
+void displayHeap() {
+    cout << "Heap: [";
+    for (int i = 0; i < num; i++) {
+        cout << heap[i];
+        if (i < num - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+int main() {
+    insert(1);
+    insert(5);
+    insert(4);
+    cout << "Initial heap:"<<endl;
+    displayHeap();
+    
+    insert(2);
+    cout << "After adding 2:"<<endl;
+    displayHeap();
+
+    return 0;
+}
